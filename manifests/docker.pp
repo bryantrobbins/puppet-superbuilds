e0c0424c87b8bcc124777d65704631ed6d8485f8
Bryan Robbins <bryan.robbins@finra.org>
2016-02-15 15:17:25 -0500

    Add sep manifest for Docker

---
+++ manifests/docker.pp
@@ -0,0 +1,11 @@
+class superbuilds::docker (
+) {
+
+  include docker
+
+  user { 'ec2-user':
+    ensure           => 'present',
+    groups           => 'docker',
+    require          => Package['docker'],
+  }
+}
