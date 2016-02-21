29d1060b0600b18f1f0d4c0ab02f58fcda4c660d
Bryan Robbins <bryan.robbins@finra.org>
2016-02-20 20:47:30 -0500

    Undo the docker group membership

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -3,9 +3,9 @@ class superbuilds::docker (
 
   include docker
 
-  user { 'ec2-user':
-    ensure           => 'present',
-    groups           => 'docker',
-    require          => Package['docker'],
-  }
+#  user { 'ec2-user':
+#    ensure           => 'present',
+#    groups           => 'docker',
+#    require          => Package['docker'],
+#  }
 }
