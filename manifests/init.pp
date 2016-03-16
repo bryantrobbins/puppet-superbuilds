59a7e11a3f8ecab28ef13ff98ebbece973f295bc
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-15 20:33:48 -0400

    Separate jenkins manifest; Use vars in templates; Get var values from hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,72 +42,8 @@ class superbuilds (
   include superbuilds::packer
   include superbuilds::r
   include superbuilds::nodejs
+  include superbuilds::jenkins
 
-  # The jenkinsworker is used by the hacky bootstrap script exec
-  user { 'jenkinsworker':
-    ensure           => 'present',
-    managehome       => true,
-  }
-
-  ssh_keygen { 'jenkinsworker':
-    require          => User['jenkinsworker'],
-  }
-
-  group { 'jenkins-group':
-    name         => 'jenkins',
-    ensure       => present,
-  }
-
-  user { 'jenkins-user':
-    name         => 'jenkins',
-    ensure       => present,
-    groups       => [ 'jenkins', 'docker'],
-    managehome   => false,
-    home         => '/var/lib/jenkins',
-    require      => [ Group['jenkins-group'], Package['docker'] ],
-  }
-
-  class { 'jenkins': 
-    manage_user  => false,
-    manage_group => false,
-    require      => User['jenkins-user'],
-  }
-
-  class { 'jenkins::cli_helper': }
-
-  jenkins::plugin { 'git-client': }
-  jenkins::plugin { 'ssh-credentials': }
-  jenkins::plugin { 'mailer': }
-  jenkins::plugin { 'promoted-builds': }
-  jenkins::plugin { 'matrix-project': }
-  jenkins::plugin { 'parameterized-trigger': }
-  jenkins::plugin { 'token-macro': }
-  jenkins::plugin { 'credentials': }
-  jenkins::plugin { 'scm-api': }
-  jenkins::plugin { 'git': }
-  jenkins::plugin { 'job-dsl': }
-  jenkins::plugin { 'ws-cleanup': }
-  jenkins::plugin { 'copyartifact': }
-
-  file { '/usr/lib/jenkins/jenkins-bootstrap.sh':
-    ensure           => file,
-    content          => template('superbuilds/setadmin.erb'),
-    mode             => '0700',
-    require          => Class['jenkins::cli_helper'],
-  }
-
-  file { '/usr/lib/jenkins/seed-job.xml':
-    ensure           => file,
-    content          => template('superbuilds/seed.xml.erb'),
-    mode             => '0700',
-    require          => Class['jenkins::cli_helper'],
-  }
-
-  exec { '/usr/lib/jenkins/jenkins-bootstrap.sh':
-    creates          => '/usr/lib/jenkins/jenkins-bootstrap.done',
-    require          => [ File['/usr/lib/jenkins/jenkins-bootstrap.sh'], File['/usr/lib/jenkins/seed-job.xml'] ],
-  }
-  
   class { 'gradle':
     version => '2.11',
   }
